9d999950b00ef4631cd5bf18ae147c8ab48782b1
vneld4862 <vneld4862@gmail.com>
2022-10-27 11:08:27 +0900

    집에서 작업한 첫내용

--- src/main/java/kh/study/test/Test1Application.java
+++ src/main/java/kh/study/test/Test1Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test1Application {
-
+	
 	public static void main(String[] args) {
+		//주석은 뭔가가 안됨
 		SpringApplication.run(Test1Application.class, args);
 	}
 
