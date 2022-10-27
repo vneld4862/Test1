3545bece2e33a664e7e4ed23d98cd52d30f973f8
vneld4862 <vneld4862@gmail.com>
2022-10-27 11:14:54 +0900

    2일차 강의실 내용

--- src/main/java/kh/study/test/Test1Application.java
+++ src/main/java/kh/study/test/Test1Application.java
@@ -8,7 +8,9 @@ public class Test1Application {
 	
 	public static void main(String[] args) {
 		//주석은 뭔가가 안됨
+		//주석만 조금이상해서 로딩안될때가있음 f5로 새로고침해주기
 		SpringApplication.run(Test1Application.class, args);
+		//2일차 강의실에서 작업끝,인커밍확인하기
 	}
 
 }
